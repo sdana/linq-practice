d51273f4755064cc3471f250e8eb9ce1cf180fed
Seth Dana <seth.dana@gmail.com>
2018-08-23 11:20:19 -0500

    numbers divisible by 4 or 6 complete

--- Program.cs
+++ Program.cs
@@ -17,6 +17,19 @@ namespace linq
             System.Console.WriteLine(fruit);
                 
             }
+
+
+                    // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+            15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            List<int> fourSixMultiples = numbers.Where(number => (number % 4 == 0) || (number % 6 == 0)).ToList();
+            foreach (int number in fourSixMultiples)
+            {
+                System.Console.WriteLine($"{number} is divisble by either 4 or 6");
+            }
         }
     }
 }
