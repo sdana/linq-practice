1ce39b75e6b0a87a82e34ff66a74617d27ee24cb
Seth Dana <seth.dana@gmail.com>
2018-08-23 11:17:18 -0500

    first exercise complete

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+            List<string> LFruits = (from fruit in fruits select fruit).Where(fruit => fruit[0].ToString() == "L").ToList();
+            foreach (string fruit in LFruits)
+            {
+            System.Console.WriteLine(fruit);
+                
+            }
+        }
+    }
+}
